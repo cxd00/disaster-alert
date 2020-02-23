cbd07855a36167812bf8f87cf0af54fe8af40c73
sgullapalli13 <sgullapalli13@gmail.com>
2020-02-22 20:37:32 -0500

    changed to non axios so phone request sent client side

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+let user = "u-cnrexzgaxeihkdqvgh6qe7a";
+let token = "t-l6hpngbf2sau4shhuyf2h2q";
+let secret = "epsbmp6xw3u274mninwxr6mg4hlvsvcozpgsqgi";
+let pass = "PjFfvrG4T8F6JVYf";
+
+let base = "dC1sNmhwbmdiZjJzYXU0c2hodXlmMmgycTplcHNibXA2eHczdTI3NG1uaW53eHI2bWc0aGx2c3Zjb3pwZ3NxZ2k="
+
+async function callBandwidth(){
+  try {
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", " Basic dC1sNmhwbmdiZjJzYXU0c2hodXlmMmgycTplcHNibXA2eHczdTI3NG1uaW53eHI2bWc0aGx2c3Zjb3pwZ3NxZ2k=");
+
+    var raw = JSON.stringify({"from":"+19195335013","to":"+19105995176","text":"bump it"});
+
+    var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+    };
+
+    let res = await fetch("https://api.catapult.inetwork.com/v1/users/u-cnrexzgaxeihkdqvgh6qe7a/messages", requestOptions);
+
+    console.log(res);
+  } catch (error) {
+      console.log(error);
+  }
+  
+}
+
+
 function pushNumber(){
     let num = document.getElementById("new_number").value;
     let zip = document.getElementById("new_zip").value;
@@ -12,7 +45,11 @@ function pushNumber(){
         console.log(num);
         console.log(zip);
         alert("Congrats.");
+        callBandwidth();
+        
     } else {
         alert ("Please type in a valid zip code and phone number."); 
     }
+
 }
+
