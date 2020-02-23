79a23a74a06df5e635c55513aacf14e06f246dc0
Cynthia <xcynthia@live.unc.edu>
2020-02-23 01:31:01 -0500

    API

---
+++ app.py
@@ -0,0 +1,84 @@
+from flask import Flask, render_template, jsonify
+from flask_restful import Api, Resource, reqparse
+import json
+
+app = Flask(__name__)
+api = Api(app)
+subscribers = {}
+subscribers["s"] = []
+subscribers["s"].append({
+    "number" : 9197445964,
+    "zip" : 27599
+})
+subscribers["s"].append({
+    "number" : 3363920218,
+    "zip" : 27599
+})
+subscribers["s"].append({
+    "number" : 9196567826,
+    "zip" : 27599
+})
+
+class Subscriber(Resource):
+    def get(self, number):
+
+        for s in subscribers["s"]:
+            if s["number"] == int(number):
+                return s, 200
+        return "Subscriber not found", 404
+
+    def post(self): # adds new
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("zip")
+        parser.add_argument("number")
+        args = parser.parse_args()
+
+        for s in subscribers["s"]:
+            if s["number"] == int(args["number"]):
+                return "{} has already registered".format(args["number"]), 400
+        
+        subscriber = {
+            "number": args["number"],
+            "zip": args["zip"]
+        }
+
+        subscribers["s"].append(subscriber)
+        with open("data.json", "w") as outfile:
+            json.dump(subscribers, outfile)
+        return subscriber, 201
+
+    def put(self, number): # updates
+        parser = reqparse.RequestParser()
+        parser.add_argument("zip")
+        args = parser.parse_args()
+
+        for s in subscribers["s"]:
+            if s["number"] == int(number):
+                s["zip"] = args["zip"]
+                return s, 200
+        
+        subscriber = {
+            "number": number,
+            "zip": args["zip"]
+        }
+
+        subscribers["s"].append(subscriber)
+        return subscriber, 201
+
+    def delete(self, number):
+        global subscribers
+        subscribers["s"] = [s for s in subscribers if not s["number"] == int(number)]
+        return "{} is deleted.".format(number), 200
+
+@app.route('/')
+def show():
+    return render_template('index.html')
+
+@app.route('/data', methods=["GET"])
+def getJson():
+    data = open("data.json").read()
+    return jsonify(data)
+
+api.add_resource(Subscriber, "/subscriber/")
+app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -2,12 +2,15 @@ let user = "u-cnrexzgaxeihkdqvgh6qe7a";
 let token = "t-l6hpngbf2sau4shhuyf2h2q";
 let secret = "epsbmp6xw3u274mninwxr6mg4hlvsvcozpgsqgi";
 let pass = "PjFfvrG4T8F6JVYf";
-
 let base = "dC1sNmhwbmdiZjJzYXU0c2hodXlmMmgycTplcHNibXA2eHczdTI3NG1uaW53eHI2bWc0aGx2c3Zjb3pwZ3NxZ2k="
 
 async function callBandwidth(){
   try {
-
+    let dt = await fetch('/data')
+    console.log(dt)
+    dt = await dt.json()
+    console.log(dt)
+    
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", " Basic dC1sNmhwbmdiZjJzYXU0c2hodXlmMmgycTplcHNibXA2eHczdTI3NG1uaW53eHI2bWc0aGx2c3Zjb3pwZ3NxZ2k=");
@@ -30,8 +33,7 @@ async function callBandwidth(){
   
 }
 
-
-function pushNumber(){
+async function pushNumber(){
     let num = document.getElementById("new_number").value;
     let zip = document.getElementById("new_zip").value;
     num = num.replace(/[^0-9]/g,'');
@@ -45,6 +47,18 @@ function pushNumber(){
         console.log(num);
         console.log(zip);
         alert("Congrats.");
+        data = {
+          "number" : num,
+          "zip" : zip 
+        }
+
+        let url = new URL("http://127.0.0.1:5000/subscriber/")
+        let params = {number: num, zip: zip}
+        url.search = new URLSearchParams(params).toString();
+        await fetch(url, {
+          method : "POST"
+        })
+
         callBandwidth();
         
     } else {
